0b8264e0e730bcf14590e801eb2bc512d78299a5
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-24 14:20:52 +0530

    Added:Welcome to CabInvoice

---
+++ src/main/java/com/bridgelabz/CabInvoice.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+public class CabInvoice {
+    public static final int COST_PER_KILOMETER = 10;
+    public static final int COST_PER_MINUTE = 1;
+    public static final int MINIMUM_FARE = 5;
+
+
+    public double calculateFare(double distance, double time) {
+        double fare = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+        return (fare < MINIMUM_FARE)? MINIMUM_FARE : fare;
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double aggregateFare = 0;
+        for (Ride ride: rides) {
+            aggregateFare += calculateFare(ride.getDistance(),ride.getTime());
+        }
+        return aggregateFare;
+    }
+
+    public Invoice generateInvoice(Ride[] rides) {
+        int totalRides = rides.length;
+        double totalFare = calculateFare(rides);
+        double averageFare =(totalFare/totalRides);
+        return new Invoice(totalRides,totalFare,averageFare);
+    }
+}
---
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class CabInvoiceTest {
+    CabInvoice cabInvoice;
+
+    @BeforeEach
+    public void init(){
+        cabInvoice = new CabInvoice();
+    }
+
+    @Test
+    public void givenDistanceAndTimeShouldReturnTotalFare(){
+        double fare = cabInvoice.calculateFare(20,5);
+        Assertions.assertEquals(205,fare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeShouldReturnMinimumFare(){
+        double fare = cabInvoice.calculateFare(0.2,1);
+        Assertions.assertEquals(5,fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRidesShouldReturnAggregateFare(){
+        Ride ride1 = new Ride(20,5);
+        Ride ride2 = new Ride(0.2,1);
+        Ride ride3 = new Ride(0.5,2);
+        Ride ride4 = new Ride(10,5);
+        Ride[] rides = {ride1,ride2,ride3,ride4};
+        double aggregateFare = cabInvoice.calculateFare(rides);
+        Assertions.assertEquals(322,aggregateFare);
+    }
+
+    @Test
+    public void givenMultipleRidesShouldReturnInvoice(){
+        Ride ride1 = new Ride(20,5);
+        Ride ride2 = new Ride(0.2,1);
+        Ride ride3 = new Ride(0.5,2);
+        Ride ride4 = new Ride(10,5);
+        Ride[] rides = {ride1,ride2,ride3,ride4};
+        Invoice expectedInvoice = new Invoice(4,322.0,322.0/4);
+        Invoice invoice = cabInvoice.generateInvoice(rides);
+        Assertions.assertEquals(expectedInvoice,invoice);
+    }
+}
